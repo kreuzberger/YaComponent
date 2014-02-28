a9d546e95bb165d5715930f18cf9908ca46b7a92
kreuzberger <joerg@kreuzberger.eu>
2014-02-28 21:18:37 +0100

    Bugfix plantuml creation if no executable was found

--- cmake/FindPlantUML.cmake
+++ cmake/FindPlantUML.cmake
@@ -8,33 +8,11 @@ FIND_FILE(PLANTUML_JAR
             PATHS  ${CMAKE_CURRENT_LIST_DIR}/../bin
          )
 
-if(NOT WIN32)
-  FIND_PROGRAM(PLANTUML_EXE plantuml )
-else()
-  FIND_PROGRAM(PLANTUML_EXE
-      NAMES plantuml.exe plantuml.cmd plantuml.bat
-  )
-endif()
-
-
-
-if( PLANTUML_JAR STREQUAL "" AND PLANTUML_EXE STREQUAL "")
+if( PLANTUML_JAR STREQUAL "" )
   set( PLANTUML_FOUND false )
 else()
-  if( NOT PLANTUML_EXE STREQUAL "")
-    set( PLANTUML_CMD ${PLANTUML_EXE} CACHE FILEPATH "PlantUML command")
-  else()
-    find_package(Java REQUIRED)
-    set( PLANTUML_CMD ${Java_JAVA_EXECUTEABLE} -jar ${PLANTUML_JAR} CACHE FILEPATH "PlantUML command")
-  endif()
-
   set( PLANTUML_FOUND true )
 endif()
-
-MARK_AS_ADVANCED(
-  PLANTUML_JAR
-  PLANTUML_EXE
-)
 
 SET(PLANTUML_INCLUDE_DIRS)
 SET(PLANTUML_LIBRARIES "${PLANTUML_JAR}")
